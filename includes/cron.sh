1bd49f7dd937fc91f3f7fed8ac4a7277169b4aa2
ripps818 <ripps818@gmail.com>
2024-02-15 20:24:18 -0600

    save cron in user home to fix permissions

--- includes/cron.sh
+++ includes/cron.sh
@@ -1,17 +1,19 @@
 # shellcheck disable=SC2148
 
+APP_HOME=/home/steam
+
 # Start supercronic and load crons
 function setup_crons() {
-    echo "" > cronlist    
+    echo "" > $APP_HOME/cronlist    
     ei ">>> Adding crons to Supercronic"
     if [[ -n ${BACKUP_ENABLED} ]] && [[ ${BACKUP_ENABLED} == "true" ]]; then
-        echo "${BACKUP_CRON_EXPRESSION} backup create" >> cronlist
+        echo "${BACKUP_CRON_EXPRESSION} backup create" >> $APP_HOME/cronlist
         e "> Added backup cron"
     fi
     if [[ -n ${RESTART_ENABLED} ]] && [[ ${RESTART_ENABLED} == "true" ]]; then
-        echo "${RESTART_CRON_EXPRESSION} restart" >> cronlist
+        echo "${RESTART_CRON_EXPRESSION} restart" >> $APP_HOME/cronlist
         e "> Added restart cron"
     fi
-    /usr/local/bin/supercronic -passthrough-logs cronlist &
+    /usr/local/bin/supercronic -passthrough-logs $APP_HOME/cronlist &
     es ">>> Supercronic started"
 }
